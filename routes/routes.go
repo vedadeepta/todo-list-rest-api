68f062108e787b042e8f0b93f78eb08a4f0d7657
Veda <=>
2018-04-30 18:31:33 +0530

    Index page route added

--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,20 @@
 package routes
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
 	"github.com/vedadeepta/todo-list-rest-api/controllers"
 )
 
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "API for todolist, check Readme for further instructions")
+}
+
 func Init() {
 	r := mux.NewRouter()
+	r.HandleFunc("/", index).Methods("GET")
 	r.HandleFunc("/create", controllers.CreateTodo).Methods("POST")
 	r.HandleFunc("/update", controllers.UpdateTodo).Methods("PUT")
 	r.HandleFunc("/todos", controllers.GetAllTodos).Methods("GET")
